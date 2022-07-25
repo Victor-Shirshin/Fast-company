28b7c218b02c9a5b4c1fe1dce251fc8c0654042e
victor shirshin <shirshin80@mail.ru>
2022-07-25 16:37:24 +0300

    fix: Component SearchStatus.jsx

--- src/components/searchStatus.jsx
+++ src/components/searchStatus.jsx
@@ -11,48 +11,28 @@ const SearchStatus = ({ length }) => {
       "человек тусанут",
       "Никто с тобой не тусанёт"
     ];
-    //   if (number === 0) {
-    //     return `${arr[3]}`;
-    //   }
-    //   if (number % 10 === 0 || (number > 10 && number <= 19)) {
-    //     return `${number} ${arr[2]} с тобой сегодня`;
-    //   } else if (
-    //     (number >= 2 && number <= 4) ||
-    //     (number % 10 >= 2 && number % 10 <= 4)
-    //   ) {
-    //     return `${number} ${arr[0]} с тобой сегодня`;
-    //   } else if (
-    //     (number >= 5 && number <= 9) ||
-    //     (number % 10 >= 5 && number % 10 <= 9) ||
-    //     number % 10 === 1
-    //   ) {
-    //     return `${number} ${arr[1]} с тобой сегодня`;
-    //   }
-    // };
+
+    if (number === 0) {
+      return `${arr[3]}`;
+    }
 
     let substrIndex = 0;
 
-    if (number === 0) {
-      substrIndex = 3;
-      return `${arr[substrIndex]}`;
-    }
     if (number % 10 === 0 || (number > 10 && number <= 19)) {
       substrIndex = 2;
-      return `${number} ${arr[substrIndex]} с тобой сегодня`;
     } else if (
       (number >= 2 && number <= 4) ||
       (number % 10 >= 2 && number % 10 <= 4)
     ) {
       substrIndex = 0;
-      return `${number} ${arr[substrIndex]} с тобой сегодня`;
     } else if (
       (number >= 5 && number <= 9) ||
       (number % 10 >= 5 && number % 10 <= 9) ||
       number % 10 === 1
     ) {
       substrIndex = 1;
-      return `${number} ${arr[substrIndex]} с тобой сегодня`;
     }
+    return `${number} ${arr[substrIndex]} с тобой сегодня`;
   };
 
   return (
